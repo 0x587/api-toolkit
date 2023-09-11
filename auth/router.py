16d10a6f118219ddf20e52a2279f760809134f54
0x587 <3225678419@qq.com>
2023-09-11 17:34:46 +0800

    补齐group路由

--- auth/router.py
+++ auth/router.py
@@ -1,23 +1,58 @@
 from fastapi import APIRouter, Depends
 from fastapi_users import FastAPIUsers
 from fastapi_users.authentication import AuthenticationBackend
+from pydantic import UUID4
+from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from .config import AuthConfigBase
 
 
 class GroupRouter(APIRouter):
-    def __init__(self, config: AuthConfigBase, get_async_session, **kwargs):
+    def __init__(self, config: AuthConfigBase, fastapi_users: FastAPIUsers, get_async_session, **kwargs):
         self._get_async_session = get_async_session
         self._config = config
         super().__init__(**kwargs)
+
+        self.add_api_route(
+            path="/",
+            endpoint=self._get_all(),
+            methods=["GET"],
+            tags=config.group_tags or ["auth"],
+            dependencies=[Depends(fastapi_users.current_user(active=True, superuser=True))],
+        )
+
+        self.add_api_route(
+            path="/{group_id}",
+            endpoint=self._delete(),
+            methods=["DELETE"],
+            tags=config.group_tags or ["auth"],
+            dependencies=[Depends(fastapi_users.current_user(active=True, superuser=True))],
+        )
 
         self.add_api_route(
             path="/register",
             endpoint=self._register(),
             methods=["POST"],
             tags=config.group_tags or ["auth"],
+            dependencies=[Depends(fastapi_users.current_user(active=True, superuser=True))],
         )
+
+    def _get_all(self):
+        async def get_all(db: AsyncSession = Depends(self._get_async_session)):
+            return (await db.exec(select(self._config.GroupDB))).all()
+
+        return get_all
+
+    def _delete(self):
+        async def delete(group_id: UUID4,
+                         db: AsyncSession = Depends(self._get_async_session)):
+            group = await db.get(self._config.GroupDB, group_id)
+            await db.delete(group)
+            await db.commit()
+            return group
+
+        return delete
 
     def _register(self):
         async def register(group: self._config.GroupCreate,  # noqa
@@ -66,7 +101,7 @@ class AuthRouter(APIRouter):
         )
 
         self.include_router(
-            GroupRouter(config, get_async_session),
+            GroupRouter(config, fastapi_users, get_async_session),
             prefix="/groups",
             tags=config.group_tags or ["auth"],
         )

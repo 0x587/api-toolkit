65be533dc7da836ad92dc8bbba5edb209dfbf8b1
0x587 <3225678419@qq.com>
2023-09-18 13:50:03 +0800

    temp

--- example.py
+++ example.py
@@ -1,14 +1,47 @@
+import json
+from typing import Tuple
+
 import uvicorn
 from fastapi import FastAPI
+from sqlalchemy import func, select
+from sqlalchemy.orm import Bundle
 
 from api_toolkit.orm import BaseORM
 from models import *
 
 from db import engine, get_db
+from schemas import *
 
 Base.metadata.create_all(engine)
 
-orm = BaseORM(next(get_db()), Ratee)
+ratee_orm = BaseORM(next(get_db()), RateeDB)
+rate_record_orm = BaseORM(next(get_db()), RateRecordDB)
+
+# r = ratee_orm.get_all().first()
+# # print(json.dumps(Ratee(**r.__dict__)))
+# print(r.__dict__)
+# print(Ratee(**r.__dict__))
+# print(rate_record_orm.get_one(('8c621d20-c0c6-4cb2-99d4-389d614c5020', 'ed6d60fd-da19-470a-9aef-64ba9fa7b1e2')))
+
+db = next(get_db())
+
+stmt = (
+    select(func.min(RateeDB.name).label('ratee_name'),
+           func.min(ProjectDB.name).label('project_name'),
+           func.avg(RateRecordDB.value).label('value'))
+    .group_by(RateRecordDB.ratee_id, RateRecordDB.project_id)
+    .join(RateeDB)
+    .join(ProjectDB)
+)
+
+
+print(stmt)
+
+for row in db.execute(stmt):
+    print(RateRecordDetail(**row._mapping))
+    # print(RateRecordDetail.model_validate(**dict(row)))
+
+# print(RateRecordDetail.from_orm(**records[0].__dict__))
 
 app = FastAPI()
 
--- models.py
+++ models.py
@@ -12,36 +12,36 @@ class Sex(IntEnum):
     Female = 1
 
 
-class Ratee(MappedAsDataclass, Base):
+class RateeDB(MappedAsDataclass, Base):
     __tablename__ = 'ratee'
 
     id: Mapped[UUID4] = mapped_column(UUID, primary_key=True, default_factory=uuid.uuid4)
     name: Mapped[str] = mapped_column(String(255), default='')
     sex: Mapped[Sex] = mapped_column(Enum(Sex), default=Sex.Female)
 
-    rate_records: Mapped[list['RateRecord']] = relationship(
-        'RateRecord', back_populates='ratee', default_factory=list)
+    rate_records: Mapped[list['RateRecordDB']] = relationship(
+        'RateRecordDB', back_populates='ratee', default_factory=list)
 
 
-class Project(MappedAsDataclass, Base):
+class ProjectDB(MappedAsDataclass, Base):
     __tablename__ = 'project'
 
     id: Mapped[UUID4] = mapped_column(UUID, primary_key=True, default_factory=uuid.uuid4)
     name: Mapped[str] = mapped_column(String(255), default='')
 
-    rate_records: Mapped[list['RateRecord']] = relationship(
-        'RateRecord', back_populates='project', default_factory=list)
+    rate_records: Mapped[list['RateRecordDB']] = relationship(
+        'RateRecordDB', back_populates='project', default_factory=list)
 
 
-class RateRecord(MappedAsDataclass, Base):
+class RateRecordDB(MappedAsDataclass, Base):
     __tablename__ = 'rate_record'
 
-    ratee_id: Mapped[UUID4] = mapped_column(ForeignKey(Ratee.id), primary_key=True, default=None)
-    ratee: Mapped[Ratee] = relationship(
-        Ratee, back_populates='rate_records', default=None)
+    ratee_id: Mapped[UUID4] = mapped_column(ForeignKey(RateeDB.id), primary_key=True, default=None)
+    ratee: Mapped[RateeDB] = relationship(
+        RateeDB, back_populates='rate_records', default=None)
 
-    project_id: Mapped[UUID4] = mapped_column(ForeignKey(Project.id), primary_key=True, default=None)
-    project: Mapped[Project] = relationship(
-        Project, back_populates='rate_records', default=None)
+    project_id: Mapped[UUID4] = mapped_column(ForeignKey(ProjectDB.id), primary_key=True, default=None)
+    project: Mapped[ProjectDB] = relationship(
+        ProjectDB, back_populates='rate_records', default=None)
 
     value: Mapped[float] = mapped_column(default=0.0)
